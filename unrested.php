02d58c5c6ccb5bcb2be9f0497981f6d0974ab031
Matt Mills <mmills@2bn.net>
2016-05-20 02:52:20 -0600

    Initial commit

---
+++ unrested.php
@@ -0,0 +1,57 @@
+<?php
+
+
+class unrested {
+	private $registered_functions = array();
+
+	public function register($method, $identifier, $function){
+		if(!isset($this->registered_functions[$method])){
+			$this->registered_functions[$method] = array();
+		}
+
+		$identifier_interim = str_replace('/', '\/', $identifier);
+		$identifier_interim = preg_replace('/{string:(?<variable_name>[^}]+)}/mi','(?<$1>[^\/?]*)',$identifier_interim);
+		$identifier_interim = preg_replace('/{integer:(?<variable_name>[^}]+)}/mi', '(?<$1>\d+)', $identifier_interim);
+		
+		$identifier_regex = '/\/api\/v(?<_api_version>\d+)' . $identifier_interim . '(\?(?<_query_param>.*))?$/mi';
+
+
+		$this_function = array(
+			'identifier_string' => $identifier,
+			'identifier_regex'  => $identifier_regex,
+			'function' 	    => $function,
+		);
+
+		$this->registered_functions[$method][] = $this_function;
+
+	}
+
+	public function run(){
+		if(!isset($this->registered_functions[$_SERVER['REQUEST_METHOD']])){
+			http_response_code(501);
+			exit;
+		}
+
+		foreach($this->registered_functions[$_SERVER['REQUEST_METHOD']] as $function){
+			$matches = array();
+			if(preg_match($function['identifier_regex'], $_SERVER['REQUEST_URI'], $matches)){
+
+				$http_body = file_get_contents('php://input');
+				$json = json_decode($http_body);
+				if(!is_array($json)){
+					$json = array();
+				}
+
+				$input = array_merge($_GET, $_POST, $json, $matches);
+
+				$result = $function['function']($input);
+				http_response_code($result[0]);
+				echo json_encode($result[1]);
+				exit;
+			}
+		}
+
+		http_response_code(501);
+	}
+}
+
